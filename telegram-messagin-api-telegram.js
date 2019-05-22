482827d00fbaacd024495d9db3aa071e0e37c980
Rodrigo Galvão Patriota <31571350+gabriel12345678901@users.noreply.github.com>
2019-05-22 13:41:23 -0300

    bot telegram

---
+++ telegram-messagin-api-telegram.js
@@ -0,0 +1,98 @@
+
+//https://www.npmjs.com/package/messaging-api-telegram
+
+const { TelegramClient } = require('messaging-api-telegram');
+const client = TelegramClient.connect('863878594:AAE95kZdNGyn1CVzdETsj0Gt0dl37JdMCfI');
+const CHAT_IDD = TelegramClient.connect('229336404');
+const CHAT_ID = '229336404';
+
+client.getWebhookInfo().catch(error => {
+  console.log(error); //s formatted error message
+  console.log(error.stack); // error stack trace
+  console.log(error.config); // axios request config
+  console.log(error.request); // HTTP request
+  console.log(error.response); // HTTP response
+
+
+
+
+
+});
+
+
+//ENVIAR MENSAGEM
+client.sendMessage(CHAT_ID, 'hi aaaagora', {
+  disable_web_page_preview: true,
+  disable_notification: true,
+});
+
+client.editMessageLiveLocation(
+  {
+    latitude: 30,
+    longitude: 45,
+  },
+  {
+    message_id: 5,
+  }
+);
+
+//client
+//  .getFile('UtAqweADGTo4Gz8cZAeR-ouu4XBx78EeqRkABPL_pM4A1UpI0koD65K2')
+//  .then(file => {
+//    console.log(file);
+   
+//  });
+
+
+
+//client.sendVideo(CHAT_ID, 'https://youtu.be/7ZoNJ38HWp4', {
+//  caption: 'gooooooodVideo',
+//  disable_notification: true,
+//});
+
+
+
+
+//client.sendDocument(CHAT_ID, 'https://www.google.com.br/url?sa=i&source=images&cd=&ved=2ahUKEwiN9s7Sk63iAhU7GbkGHY5UAAwQjRx6BAgBEAU&url=https%3A%2F%2Fserede.com.br%2F&psig=AOvVaw0UMidlq95WTUQI98UiVFVk&ust=1558546324405911', {
+//  caption: 'gooooooodDocument',
+//  disable_notification: true,
+//});
+
+
+client
+  .getUpdates({
+    limit: 10,
+  })
+  .then(updates => {
+    console.log(updates);
+
+   });
+
+  
+ 
+  //client.getMe().then(result => {
+  //console.log(result);
+  
+  //});
+
+  //client.sendContact(
+  //CHAT_ID,
+  //{
+  //  phone_number: '886123456789',
+  //  first_name: 'first',
+  //},
+  //{ last_name: 'last' }
+  //);
+
+  //client.sendChatAction(CHAT_ID, 'typing');
+
+//console.log('chat id: '+CHAT_ID);
+//  client.getChat(CHAT_ID).then(chat => {
+//  console.log(chat);
+  
+//  });
+
+
+
+
+
